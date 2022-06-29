6d557b7d03b990e3b8dcc1f5e16d6126ecd80fcd
Demitoye <ologuntoye@systemspecs.com.ng>
2022-06-29 10:15:54 +0100

    after second pulish

--- src/lib/components/Empty.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {View, TextInput} from 'react-native';
-
-function Input(props) {
-  return <TextInput placeholder={props.placeholder} />;
-}
-
-export default Input;
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,3 @@
 import Badge from './components/Badge';
 import Button from './components/Button';
-import Input from './components/empty';
-export { Badge, Button ,Input};
+export { Badge, Button };
